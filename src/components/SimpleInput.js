608ab6c3ff235145e46a6a37f9b23b98f81efc7a
Peto Gurin <no.mi83@yahoo.com.vn>
2023-05-16 20:42:31 +0700

    Only 1 input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,43 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  //const inputRef = useRef()
+  const [enteredInput, setInput] = useState('')
+  const [touched,setTouch] = useState(false)
+
+  const enteredValid = enteredInput.trim() !== ""
+  const formSubmitted = !enteredValid && touched
+  const nameInputChangeHandler = event => {
+    setInput(event.target.value)
+  }
+
+  const formHandler = (e) => {
+    e.preventDefault();
+    setTouch(true)
+    if(!enteredValid){
+      return
+    }
+    console.log(enteredInput);
+    setInput("")
+    setTouch(false)
+  }
+
+  const nameInputBlurHandler = (e) => {
+    setTouch(true)
+    if(!enteredValid){
+      return
+    }
+  }
+  
+  const formClasses = formSubmitted ? "form-control invalid" : "form-control "
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={formHandler}>
+      <div className={formClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input value={enteredInput} type='text' id='name' onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler}/>
+        {formSubmitted && <p className="error-text ">Name must be type!</p>}
       </div>
-      <div className="form-actions">
+      <div className="form-actions">  
         <button>Submit</button>
       </div>
     </form>
